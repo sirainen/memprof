e3b465141dd75625aae71189d9ebba7be39e3801
Owen Taylor <otaylor@redhat.com>
2000-05-24 00:13:28 +0000

    Link against -lbfd and -liberty statically, since the so's change with
    Wed Jan 12 10:42:35 2000  Owen Taylor  <otaylor@redhat.com>
    
    	* configure.in (MEMPROF_LIBS): Link against -lbfd and
    	-liberty statically, since the so's change with every
    	release of binutils.
    
    	* testmemprof.c: Make a bit more interesting.

--- testmemprof.c
+++ testmemprof.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void *a()
+{
+  return malloc(1000);
+}
+
+void *b()
+{
+  return a();
+}
+
 int main()
 {
   char *block;
-  
-  block = malloc(1000);
+
+  block = b();
   block = malloc(1000);
   free(block);
 
